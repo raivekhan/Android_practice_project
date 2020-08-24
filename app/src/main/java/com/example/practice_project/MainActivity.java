559e6fe2caa9ab7ea549cd6a4649b9bc31f64a66
Naimur <raivekhan@gmail.com>
2020-08-25 01:22:56 +0600

    Heading: Added button and implement onclick with listener

--- app/src/main/java/com/example/practice_project/MainActivity.java
+++ app/src/main/java/com/example/practice_project/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.practice_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    String TAG = "Practice_project";
+    Button button1;
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1= findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        button2=findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.button1){
+            Log.d(TAG, "button 1 clicked");
+        }
+        else {
+            Log.d(TAG, "button 2 clicked");
+        }
     }
 }
