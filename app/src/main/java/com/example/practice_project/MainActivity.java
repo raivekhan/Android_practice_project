8180f1f969286a6d4fc56d0e9733797541fc577d
Naimur <raivekhan@gmail.com>
2020-09-21 00:35:28 +0600

    Heading: Open another activity on click button with intent

--- app/src/main/java/com/example/practice_project/MainActivity.java
+++ app/src/main/java/com/example/practice_project/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.practice_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void navigateToSecondScreen(View view) {
+        Intent navigateIntent=new Intent(this, Main2Activity.class);
+        startActivity(navigateIntent);
     }
 }
